9ab87b369390ace5a79cfd4690756f63f8368613
tanwlanyue <tanwlanyue@gmail.com>
2021-11-03 17:46:28 +0800

    update

---
+++ springboot/src/main/java/com/tanwlanyue/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.tanwlanyue.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanglei211
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
